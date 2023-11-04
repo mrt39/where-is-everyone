f53d5ec2021b2029d94e3843dda61427619163cb
mrt39 <mertoktar39@gmail.com>
2023-11-04 14:01:15 +0300

    Change mouse cursor and make image marker responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { Outlet } from "react-router-dom";
 import './styles/App.css'
 import Navbar from "./components/Navbar.jsx"
 import Footer from './components/Footer.jsx';
-
-
+import {targetCharactersWithCoordinates} from './components/CharacterData.js';
 
 
 const App = () => {
@@ -22,27 +21,6 @@ const App = () => {
   // state to check whether stopwatch is running
   const [isRunning, setIsRunning] = useState(false);
 
-  const targetCharactersWithCoordinates = {
-  "star-wars": {"Darth Vader": {"X": [0.9053, 0.9542], "y": [0.3491, 0.4051]},
-  "Mace Windu" : {"X":[0.1292, 0.1899] , "y":[0.4565, 0.5112]},
-  "Chewbacca" : {"X":[0.8078, 0.8659] , "y": [0.8093, 0.8549]},
-  "Mas Amedda": {"X":[0.2679, 0.3185] , "y":[0.6011, 0.6660]},
-  "Obi-Wan": {"X":[0.2101, 0.2553] , "y":[0.1458, 0.2026]}},
-
-  "nozze-cana": {"The Bride": {"X": [0.0594, 0.0832], "y": [0.6564, 0.7864] },
-  "Paolo Veronese" : {"X": [0.4294, 0.4770], "y": [0.6892, 0.7633] },
-  "The Butcher" : {"X": [0.4943, 0.5727], "y": [0.4227, 0.5111] },
-  "Hourglass (Vanity)": {"X": [0.4845, 0.5193], "y": [0.7941, 0.8480] },
-  "Suleiman the Magnificent" : {"X": [0.1245, 0.1781], "y": [0.6073, 0.7124] },},
-
-  "festival": {"Robin Hood": {"X": [0.8289, 0.9002], "y": [0.6178, 0.6933] },
-  "Witch" : {"X": [0.1089, 0.1786], "y": [0.6418, 0.7223] },
-  "Adam" : {"X": [0.3754, 0.4278], "y": [0.1075, 0.1818]},
-  "Erato": {"X": [0.8537, 0.9197], "y": [0.1609, 0.2423] },
-  "Edgar Allen Poe" : {"X": [0.2624, 0.3218], "y": [0.8483, 0.9184] },},
-
-  
-}
 
   return (
     <div className='appContainer'>
@@ -66,4 +44,5 @@ const App = () => {
 };
 
 export default App;
+
 
